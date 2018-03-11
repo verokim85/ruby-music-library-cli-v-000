23731c22ebf31d83731bc6edacaafcd8f73a534d
Veronica Kim <vero.a.kim@gmail.com>
2018-03-11 23:18:03 +0000

    automatically backed up by learn

--- lib/MusicLibraryController.rb
+++ lib/MusicLibraryController.rb
@@ -30,8 +30,8 @@ class MusicLibraryController
           list_songs
         when "list artists"
           list_artists
-        when "list genres"
-          list_genres
+        when "list genre"
+          list_genre
         when 'list songs by artist'
           list_songs_by_artist
         when 'list songs by genre'
