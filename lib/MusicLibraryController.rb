c10a9c4dc4bac540d3480a31fe20d3566237e541
Veronica Kim <vero.a.kim@gmail.com>
2018-03-11 22:41:06 +0000

    automatically backed up by learn

--- lib/MusicLibraryController.rb
+++ lib/MusicLibraryController.rb
@@ -98,8 +98,7 @@ end
     puts "Which song number would you like to play?"
 
     input = gets.strip
-    if input <= Song.all.split("").length
-
+    
     song = Song.all.detect {|song| song.name== input}
 
       song.each_with_index do |song, index|
